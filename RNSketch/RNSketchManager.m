35272ef5c54659b6e6b96f472b6c6e7a7d861e16
Chris Cabral <vccabral@gmail.com>
2017-05-22 14:51:12 -0400

    Update RNSketchManager.m

--- RNSketch/RNSketchManager.m
+++ RNSketch/RNSketchManager.m
@@ -1,5 +1,5 @@
 //
-//  RNSketchManager.m
+//  RNSketch.m
 //  RNSketch
 //
 //  Created by Jeremy Grancher on 28/04/2016.
@@ -12,96 +12,282 @@
 #import "RNSketch.h"
 #import "RNSketchManager.h"
 
-#define ERROR_IMAGE_INVALID @"ERROR_IMAGE_INVALID"
-#define ERROR_FILE_CREATION @"ERROR_FILE_CREATION"
-
-@implementation RNSketchManager
-
-RCT_EXPORT_MODULE()
-
-#pragma mark - Properties
-
-
-RCT_CUSTOM_VIEW_PROPERTY(fillColor, UIColor, RNSketch)
+@implementation RNSketch
 {
-  [view setFillColor:json ? [RCTConvert UIColor:json] : [UIColor whiteColor]];
-}
-RCT_CUSTOM_VIEW_PROPERTY(strokeColor, UIColor, RNSketch)
-{
-  [view setStrokeColor:json ? [RCTConvert UIColor:json] : [UIColor blackColor]];
-}
-RCT_CUSTOM_VIEW_PROPERTY(clearButtonHidden, BOOL, RNSketch)
-{
-  [view setClearButtonHidden:json ? [RCTConvert BOOL:json] : NO];
-}
-RCT_EXPORT_VIEW_PROPERTY(strokeThickness, NSInteger)
-
-#pragma mark - Lifecycle
-
-- (instancetype)init
-{
-  if ((self = [super init])) {
-    self.sketchView = nil;
-  }
-  
-  return self;
-}
-
-- (UIView *)view
-{
-  if (!self.sketchView) {
-    self.sketchView = [[RNSketch alloc] initWithEventDispatcher:self.bridge.eventDispatcher];
-  }
-  
-  return self.sketchView;
-}
-
-#pragma mark - Event types
-
-
-- (NSArray *)customDirectEventTypes
-{
-  return @[
-           @"onReset",
-           ];
+    // Internal
+    RCTEventDispatcher *_eventDispatcher;
+    UIButton *_clearButton;
+    UIBezierPath *_path;
+    NSArray *bezierPointsArray; //added as global variable to track all points
+    UIImage *_image;
+    CGPoint _points[5];
+    uint _counter;
+    
+    // Configuration settings
+    UIColor *_fillColor;
+    UIColor *_strokeColor;
 }
 
 
-#pragma mark - Exported methods
+#pragma mark - UIViewHierarchy methods
 
-RCT_EXPORT_METHOD(saveImage:(NSString *)encodedImage
-                  resolve:(RCTPromiseResolveBlock)resolve
-                  reject:(RCTPromiseRejectBlock)reject)
+
+- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher
 {
-  // Create image data with base64 source
-  NSData *imageData = [[NSData alloc] initWithBase64EncodedString:encodedImage options:NSDataBase64DecodingIgnoreUnknownCharacters];
-  if (!imageData) {
-    return reject(ERROR_IMAGE_INVALID, @"You need to provide a valid base64 encoded image.", nil);
-  }
-
-  // Create full path of image
-  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-  NSString *documentsDirectory = [paths firstObject];
-  NSFileManager *fileManager = [NSFileManager defaultManager];
-  NSString *fullPath = [[documentsDirectory stringByAppendingPathComponent:[[NSUUID UUID] UUIDString]] stringByAppendingPathExtension:@"jpg"];
-
-  // Save image and return the path
-  BOOL fileCreated = [fileManager createFileAtPath:fullPath contents:imageData attributes:nil];
-  if (!fileCreated) {
-    return reject(ERROR_FILE_CREATION, @"An error occured. Impossible to save the file.", nil);
-  }
-  resolve(@{@"path": fullPath});
+    if ((self = [super init])) {
+        // Internal setup
+        self.multipleTouchEnabled = NO;
+        // For borderRadius property to work (CALayer's cornerRadius).
+        self.layer.masksToBounds = YES;
+        _eventDispatcher = eventDispatcher;
+        _path = [UIBezierPath bezierPath];
+        
+        [self initClearButton];
+    }
+    
+    return self;
 }
 
-RCT_EXPORT_METHOD(clear)
+- (void)layoutSubviews
 {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [self.sketchView clearDrawing];
-  });
+    [super layoutSubviews];
+    [self drawBitmap];
 }
 
-RCT_EXPORT_METHOD(get_points:(RCTResponseSenderBlock)callback) {
-    callback(@[[NSNull null],[self.sketchView getAllPoints]]);
+- (void)setClearButtonHidden:(BOOL)hidden
+{
+    _clearButton.hidden = hidden;
+}
+
+
+#pragma mark - Subviews
+
+
+- (void)initClearButton
+{
+    // Clear button
+    CGRect frame = CGRectMake(0, 0, 40, 40);
+    _clearButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    _clearButton.frame = frame;
+    _clearButton.enabled = false;
+    _clearButton.tintColor = [UIColor blackColor];
+    _clearButton.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [_clearButton setTitle:@"x" forState:UIControlStateNormal];
+    [_clearButton addTarget:self action:@selector(clearDrawing) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Clear button background
+    UIButton *background = [UIButton buttonWithType:UIButtonTypeCustom];
+    background.frame = frame;
+    
+    // Add subviews
+    [self addSubview:background];
+    // [self addSubview:_clearButton];
+}
+
+
+#pragma mark - UIResponder methods
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    _counter = 0;
+    UITouch *touch = [touches anyObject];
+    _points[0] = [touch locationInView:self];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    _counter++;
+    UITouch *touch = [touches anyObject];
+    _points[_counter] = [touch locationInView:self];
+    
+    if (_counter == 4) [self drawCurve];
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    // Enabling to clear
+    [_clearButton setEnabled:true];
+    
+    [self drawBitmap];
+    [self setNeedsDisplay];
+    //get all points before removing the path
+    bezierPointsArray = [self getAllPoints];
+    
+    [_path removeAllPoints];
+    _counter = 0;
+    
+    // Send event
+    NSDictionary *bodyEvent = @{
+                                @"target": self.reactTag,
+                                @"image": [self drawingToString],
+                                };
+    [_eventDispatcher sendInputEventWithName:@"topChange" body:bodyEvent];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self touchesEnded:touches withEvent:event];
+}
+
+
+#pragma mark - UIViewRendering methods
+
+
+- (void)drawRect:(CGRect)rect
+{
+    [_image drawInRect:rect];
+    [_strokeColor setStroke];
+    [_path stroke];
+}
+
+
+#pragma mark - Drawing methods
+
+
+- (void)drawCurve
+{
+    // Move the endpoint to the middle of the line
+    _points[3] = CGPointMake((_points[2].x + _points[4].x) / 2, (_points[2].y + _points[4].y) / 2);
+    
+    [_path moveToPoint:_points[0]];
+    [_path addCurveToPoint:_points[3] controlPoint1:_points[1] controlPoint2:_points[2]];
+    
+    [self setNeedsDisplay];
+    
+    // Replace points and get ready to handle the next segment
+    _points[0] = _points[3];
+    _points[1] = _points[4];
+    _counter = 1;
+}
+
+- (void)drawBitmap
+{
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, NO, 0);
+    
+    // If first time, paint background
+    if (!_image) {
+        [_fillColor setFill];
+        [[UIBezierPath bezierPathWithRect:self.bounds] fill];
+    }
+    
+    // Draw with context
+    [_image drawAtPoint:CGPointZero];
+    [_strokeColor setStroke];
+    [_path stroke];
+    _image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+}
+
+
+- (NSMutableArray *) getAllPoints
+{
+    //_path
+    NSMutableArray *points = [NSMutableArray array];
+    CGPathApply(_path.CGPath, (__bridge void *)points, getPointsFromBezier);
+    
+    return points;
+}
+
+- (NSArray * ) getBezierPointsArray{
+    return bezierPointsArray;
+}
+
+void getPointsFromBezier (void *info, const CGPathElement *element)
+{
+    NSMutableArray *bezierPoints = (__bridge NSMutableArray *)info;
+    
+    CGPoint *points = element->points;
+    CGPathElementType type = element->type;
+    NSMutableDictionary *pointsDictionary = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *points2Dictionary = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *points3Dictionary = [[NSMutableDictionary alloc] init];
+    switch(type) {
+        case kCGPathElementMoveToPoint:
+        case kCGPathElementAddLineToPoint: 
+                [pointsDictionary setValue:[[NSNumber alloc] initWithFloat:points[0].x] forKey:@"x"];
+                [pointsDictionary setValue:[[NSNumber alloc] initWithFloat:points[0].y] forKey:@"y"];
+                [bezierPoints addObject:pointsDictionary];
+            break;
+            
+        case kCGPathElementAddQuadCurveToPoint: // contains 2 points
+              [pointsDictionary setValue:[[NSNumber alloc] initWithFloat:points[0].x] forKey:@"x"];
+              [pointsDictionary setValue:[[NSNumber alloc] initWithFloat:points[0].y] forKey:@"y"];
+              [bezierPoints addObject:pointsDictionary];
+              [pointsDictionary2 setValue:[[NSNumber alloc] initWithFloat:points[1].x] forKey:@"x"];
+              [pointsDictionary2 setValue:[[NSNumber alloc] initWithFloat:points[1].y] forKey:@"y"];
+              [bezierPoints addObject:pointsDictionary2];
+            break;
+            
+        case kCGPathElementAddCurveToPoint: // contains 3 points
+              [pointsDictionary setValue:[[NSNumber alloc] initWithFloat:points[0].x] forKey:@"x"];
+              [pointsDictionary setValue:[[NSNumber alloc] initWithFloat:points[0].y] forKey:@"y"];
+              [bezierPoints addObject:pointsDictionary];
+              [pointsDictionary2 setValue:[[NSNumber alloc] initWithFloat:points[1].x] forKey:@"x"];
+              [pointsDictionary2 setValue:[[NSNumber alloc] initWithFloat:points[1].y] forKey:@"y"];
+              [bezierPoints addObject:pointsDictionary2];
+              [pointsDictionary2 setValue:[[NSNumber alloc] initWithFloat:points[2].x] forKey:@"x"];
+              [pointsDictionary2 setValue:[[NSNumber alloc] initWithFloat:points[2].y] forKey:@"y"];
+              [bezierPoints addObject:pointsDictionary3];
+            break;
+            
+        case kCGPathElementCloseSubpath: // contains no point
+            break;
+    }
+}
+
+#pragma mark - Export drawing
+
+
+- (NSString *)drawingToString
+{
+    return [UIImagePNGRepresentation(_image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+}
+
+
+#pragma mark - Clear drawing
+
+
+- (void)clearDrawing
+{
+    // Disabling to clear
+    [_clearButton setEnabled:false];
+    
+    _image = nil;
+    
+    [self drawBitmap];
+    [self setNeedsDisplay];
+    
+    // Send event
+    NSDictionary *bodyEvent = @{
+                                @"target": self.reactTag,
+                                };
+    [_eventDispatcher sendInputEventWithName:@"onReset" body:bodyEvent];
+}
+
+
+- (int)score
+{
+    return 0;
+}
+
+#pragma mark - Setters
+
+
+- (void)setFillColor:(UIColor *)fillColor
+{
+    _fillColor = fillColor;
+}
+
+- (void)setStrokeColor:(UIColor *)strokeColor
+{
+    _strokeColor = strokeColor;
+}
+
+- (void)setStrokeThickness:(NSInteger)strokeThickness
+{
+    _path.lineWidth = strokeThickness;
 }
 
 @end
